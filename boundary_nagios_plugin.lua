bf9090f7493c0c88da3b52715ace37d275e701c1
jdgwartney <davidg@boundary.com>
2014-08-18 16:09:06 -0700

    Add setting of seed for random number generator

--- boundary_nagios_plugin.lua
+++ boundary_nagios_plugin.lua
@@ -1,11 +1,11 @@
-#!/usr/bin/lua
+#!/usr/bin/env lua
 
 function getRandomValue(lower, upper)
    return math.random(lower,upper*100)/100
 end
 
 function Main()
-
+math.randomseed(os.time())
 print("LOAD_1_MINUTE",getRandomValue(0,20))
 print("LOAD_5_MINUTE",getRandomValue(0,20))
 print("LOAD_15_MINUTE",getRandomValue(0,20))
