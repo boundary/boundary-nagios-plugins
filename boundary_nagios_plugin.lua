c7057115a528e1e5cf0f570715fdce1b4794ea67
jdgwartney <davidg@boundary.com>
2014-08-19 11:07:58 -0700

    Checkpoint of work to date

--- boundary_nagios_plugin.lua
+++ boundary_nagios_plugin.lua
@@ -1,20 +1,24 @@
 #!/usr/bin/env lua
-s = require("string")
+string = require("string")
+checkLoad = require("check_load")
 
 function getRandomValue(lower, upper)
-   return math.random(lower,upper)
+  return math.random(lower,upper)
 end
 
 function Main()
-local file = assert(io.popen('hostname', 'r'))
-local hostname = file:read('*all')
-hostname = s.gsub(hostname, "\n","")
-file:close()
-math.randomseed(os.time())
+--  local file = assert(io.popen('hostname', 'r'))
+--  local hostname = file:read('*all')
+--  hostname = string.gsub(hostname, "\n","")
+--  file:close()
+--  math.randomseed(os.time())
+--
+--  io.write("LOAD_1_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+--  io.write("LOAD_5_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+--  io.write("LOAD_15_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+check = checkLoad.newCheckLoad()
 
-io.write("LOAD_1_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
-io.write("LOAD_5_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
-io.write("LOAD_15_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+check.getMetrics(io)
 
 end
 
