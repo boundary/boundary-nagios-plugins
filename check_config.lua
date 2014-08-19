3c08a1f4bc555de1fa5f57e157d71c392620cecc
jdgwartney <davidg@boundary.com>
2014-08-19 14:22:30 -0700

    First set of unit tests

--- check_config.lua
+++ check_config.lua
@@ -1,23 +1,39 @@
 json = require('dkjson')
 
-function newCheckConfig(filename)
-  local self = {filename}
-
-  local getConfig = function ()
+function newCheckConfig(filePath)
+  local self = {path = filePath,content}
+  
+  local readFile = function()
     -- Load the json configuration into a string
-    local f = assert(io.open(config, "r"))
+    print(self.path)
+    local f = assert(io.open(self.path, "r"))
     local t = f:read("*a")
     f:close()
+    self.content = t
+  end
 
-    local obj, pos, err = json.decode (t, 1, nil)
+  local getConfig = function ()
+    -- Decode JSON to create the configuration object
+    local obj, pos, err = json.decode (self.content, 1, nil)
     if err then
       print ("Error:", err)
     end
     return obj
   end
+
+  local getContent = function()
+    return self.content
+  end
   
+  local getPath = function()
+    return self.path
+  end
+
   return {
-    getConfig = getConfig
+    getConfig = getConfig,
+    readFile = readFile,
+    getContent = getContent,
+    getPath = getPath
   }
 end
 
--- check_config_test.lua
+++ check_config_test.lua
@@ -1,75 +1,35 @@
-function my_super_function( arg1, arg2 ) return arg1 + arg2 end
-function my_bad_function( arg1, arg2 ) return arg1 - arg2 end
+require("check_config")
 
 -- Unit testing starts
 LuaUnit = require('luaunit')
 
--- now, we perform all the tests on int in one test class
--- and the tests on float in another one
--- when your test grows, you will have many test classes
 
-TestWithInt = {} --class
-    function TestWithInt:setUp() 
-        -- this function is run before each test, so that multiple
-        -- tests can share initialisations
-        self.a = 1
-        self.b = 2
-    end
+TestCheckConfig = {}
+function TestCheckConfig:setUp()
+  self.a = 1
+  self.b = 2
+  self.file = "src/test/resources/example_config.json"
+end
 
-    function TestWithInt:tearDown() 
-        -- this function is executed after each test
-        -- here, we have nothing to do so we could have avoid
-        -- declaring it
-    end
+function TestCheckConfig:tearDown()
+  self.config = nil
+end
 
-    function TestWithInt:testSuperFunction()
-        result = my_super_function( self.a, self.b )
-        assertEquals( type(result), 'number' )
-        assertEquals( result, 3 )
-    end
+function TestCheckConfig:testGetFilePath()
+  c = newCheckConfig(self.file)
+  assertEquals(c.getPath(),self.file)
+end
 
-    function TestWithInt:testBadFunction()
-        result = my_bad_function( self.a, self.b )
-        assertEquals( type(result), 'number' )
-        assertEquals( result, -1 )
-    end
+function TestCheckConfig:testReadFile()
+  c = newCheckConfig(self.file)
+  c.readFile()
+  assertEquals(type(c.getContent()), 'string' )
+end
 
-    function TestWithInt:testThatFails()
-        -- you can test anything with assertEquals
-        assertEquals( self.a, 1 )
-        assertEquals( type(self.a), 'number' )
-        -- will fail
-        assertEquals( 'hop', 'bof' )
-    end
--- class TestWithInt
-
-TestWithFloat = {} --class
-    function TestWithFloat:setUp() 
-        -- this function is run before each test, so that multiple
-        -- tests can share initialisations
-        self.a = 1.1
-        self.b = 2.1
-    end
-
-    function TestWithFloat:tearDown() 
-        -- this function is executed after each test
-        -- here, we have nothing to do so we could have avoid
-        -- declaring it
-    end
-
-    function TestWithFloat:testSuperFunction()
-        result = my_super_function( self.a, self.b )
-        assertEquals( type(result), 'number' )
-        -- will fail
-        assertEquals( result, 3 )
-    end
-
-    function TestWithFloat:testBadFunction()
-        result = my_bad_function( self.a, self.b )
-        assertEquals( type(result), 'number' )
-        -- will work, but only by chance :-)
-        assertEquals( result, -1 )
-    end
--- class TestWithFloat
+function TestCheckConfig:testBadFunction()
+  result = 4
+  assertEquals( type(result), 'number' )
+  assertEquals( result, 4)
+end
 
 LuaUnit:run()
