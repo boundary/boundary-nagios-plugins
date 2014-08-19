ead540990404a14d9514bc2e0b7eca22c5f27cff
jdgwartney <davidg@boundary.com>
2014-08-19 11:08:46 -0700

    First cut at object definition for check_load Nagios plugin

---
+++ check_load.lua
@@ -0,0 +1,25 @@
+
+function newCheckLoad ()
+  local self = {}
+
+  local getMetrics = function (io)
+    local file = assert(io.popen('hostname', 'r'))
+    local hostname = file:read('*all')
+    hostname = string.gsub(hostname, "\n","")
+    file:close()
+    math.randomseed(os.time())
+
+    io.write("LOAD_1_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+    io.write("LOAD_5_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+    io.write("LOAD_15_MINUTE ",getRandomValue(0,20)," ",hostname,"\n")
+
+  end
+
+  return {
+    getMetrics = getMetrics
+  }
+end
+
+return {
+  newCheckLoad = newCheckLoad
+}
