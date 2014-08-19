f54ba7a9192b69b1ea3f7af55359e58e14ed8b79
jdgwartney <davidg@boundary.com>
2014-08-19 13:33:11 -0700

    Add unit test framework, configuration object

--- load_config.lua
+++ load_config.lua
@@ -1,26 +1,26 @@
 json = require('dkjson')
 
-config="check_config.json"
+function newLoadConfiguration(filename)
+  local self = {filename}
 
--- Load the json configuration into a string
-local f = assert(io.open(config, "r"))
-local t = f:read("*a")
-f:close()
+  local getConfig = function ()
+    -- Load the json configuration into a string
+    local f = assert(io.open(config, "r"))
+    local t = f:read("*a")
+    f:close()
 
-print(t)
-
-local obj, pos, err = json.decode (t, 1, nil)
-if err then
-  print ("Error:", err)
-else
-  print(obj)
-  config = obj.nagios_check_config
-  for k,v in pairs(config)
-  do
-    print(k,v)
-    for i,j in pairs(v)
-    do
-      print(i,j)
+    local obj, pos, err = json.decode (t, 1, nil)
+    if err then
+      print ("Error:", err)
     end
+    return obj
   end
+  
+  return {
+    getConfig = getConfig
+  }
 end
+
+return {
+  newLoadConfiguration = newLoadConfiguration
+}
